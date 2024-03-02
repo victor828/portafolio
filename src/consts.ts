11c919216b01bff1a09c24ed9f10e37536d67df8
Víctor Orozco <veom104@gmail.com>
2024-03-02 01:49:51 -0800

    📝 [ADD] add Home page
    is the start of my portfolio and finshed the Home page. it's simpe in this moment 🦖

---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Portafolio";
+export const SITE_DESCRIPTION =
+  "Hola soy Victor y aqui presento mi portafolio!, estuve estudiando durante medio a►4o como full stack web developer.";
