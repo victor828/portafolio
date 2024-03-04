194be0224061cbdfa5ce00ad60a6f189b788c1e4
Víctor Orozco <veom104@gmail.com>
2024-03-03 23:09:20 -0800

    update configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind(), preact()]
+  site: "https://example.com",
+  integrations: [tailwind(), preact()],
+  output: "hybrid",
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,24 @@
+import animations from "@midudev/tailwind-animations";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      "cod-gray": {
+        50: "#f6f6f6",
+        100: "#e7e7e7",
+        200: "#d1d1d1",
+        300: "#b0b0b0",
+        400: "#888888",
+        500: "#6d6d6d",
+        600: "#5d5d5d",
+        700: "#4f4f4f",
+        800: "#454545",
+        900: "#3d3d3d",
+        950: "#111111",
+      },
+    },
+  },
+  plugins: [animations],
+};
